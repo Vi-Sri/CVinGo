ab02d51cb3652fb027fbff779f5a91ce1e50d5e5
Vi-Sri <nikola.tesla195@gmail.com>
2021-04-24 10:40:56 +0530

    adding plotting

---
+++ plotting_barchart.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"log"
+)
+
+func main() {
+
+		// Create the plot values and labels.
+		values := plotter.Values{0.5, 10, 20, 30}
+		verticalLabels := []string{"A", "B", "C", "D"}
+		horizontalLabels := []string{"Label A", "Label B", "Label C", "Label D"}
+
+		// Create a vertical BarChart
+		p1 := plot.New()
+		verticalBarChart, err := plotter.NewBarChart(values, 0.5*vg.Centimeter)
+		if err != nil {
+			log.Panic(err)
+		}
+		p1.Add(verticalBarChart)
+		p1.NominalX(verticalLabels...)
+		err = p1.Save(100, 100, "verticalBarChart.png")
+		if err != nil {
+			log.Panic(err)
+		}
+
+		p2 := plot.New()
+		horizontalBarChart, err := plotter.NewBarChart(values, 0.5*vg.Centimeter)
+		horizontalBarChart.Horizontal = true // Specify a horizontal BarChart.
+		if err != nil {
+			log.Panic(err)
+		}
+		p2.Add(horizontalBarChart)
+		p2.NominalY(horizontalLabels...)
+		err = p2.Save(100, 100, "horizontalBarChart.png")
+		if err != nil {
+			log.Panic(err)
+		}
+}
